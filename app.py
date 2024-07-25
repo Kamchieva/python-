510fcb03562a96875dbea5799546cc6ad0cbe823
Aidan <150118075+Kamchieva@users.noreply.github.com>
2024-07-25 18:43:30 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+
+character_name = "Tom"
+character_age = "15"
+is_male = False
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old ,")
+character_name = "Ann"
+character_age = "45"
+print("He really liked the name " + character_name + ",")
+print("But he didn't like being " + character_age + ",")
+phrase = "Giraffe Academy"
+print(phrase + " is cool")
+print(phrase.lower())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("Academy"))
+print(phrase.replace("Giraffe", "Elephant"))
+print(2)
+print(-33)
+print(5 * 33 +566)
+print(10 % 3)
+my_num = 3
+print(my_num)
+print(str(my_num) + " my favorite number")
+print(abs(my_num))
+print(pow(9,3))
+print(max(6,9))
+print(min(6,9))
+print(round(4.5))

7136641f1069b1cab13ad1f65cdfee7eceb87fc5
Aidan <150118075+Kamchieva@users.noreply.github.com>
2024-09-16 11:34:49 -0500

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,26 @@
+from turtle import *
+
+hideturtle()
+color('black', 'magenta')
+
+
+def left_turn():
+    for i in range(10):
+        forward(15)
+        left(9)
+
+
+def petal():
+    begin_fill()
+    left_turn()
+    left(90)
+    left_turn()
+    left(90)  # Line added
+    end_fill()
+
+
+for i in range(5):
+    petal()
+    right(360 / 5)
+
+bye()
